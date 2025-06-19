bc768f330d863a9bfa72c76fea72d582d9bc2e9a
product-validation96 <114807401+akshar9662@users.noreply.github.com>
2025-06-19 11:13:50 +0530

    Create AdComponent.js

---
+++ src/Components/AdComponent.js
@@ -0,0 +1,26 @@
+// src/components/AdComponent.js
+import { useEffect } from 'react';
+
+const AdComponent = ({ slot }) => {
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error('Adsense error:', e);
+    }
+  }, []);
+
+  return (
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3463266713741263"
+     data-ad-slot="1812068633"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+  );
+};
+
+export default AdComponent;
